9a142de87939ac92acbadac6a5d9194a5d5d468e
Mercy Nani <S01221810@acad.tri-c.edu>
2019-10-24 23:02:55 -0400

    Created Lab1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Mercy Nani");
+            Console.ReadLine();
+        }
+    }
+}
