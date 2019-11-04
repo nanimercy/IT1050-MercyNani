9a7c240588ccc74823821a81e23673cf51031f77
Mercy Nani <S01221810@acad.tri-c.edu>
2019-11-03 20:49:20 -0500

    Added helloworld

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class Program
+    class Multiplication
     {
         static void Main(string[] args)
         {
+            int number1;
+            int number2;
+            int multiply;
+
+            Console.Write("Enter first integer: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            multiply = number1 * number2;
+
+            Console.WriteLine("Multiplication is {0}", multiply);
+            Console.ReadLine();
+
+            Console.WriteLine("{0}\n{1}", "HelloWorld!", "From Mercy Nani");
+            Console.ReadLine();
+
+            Console.WriteLine("HelloWorld!   From Mercy Nani");
+            Console.ReadLine();
 
         }
+
     }
 }
+
+
