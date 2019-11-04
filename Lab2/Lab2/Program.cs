e0edf8aa9fe2751077bf8e579088d9e5a8b3fc4a
Mercy Nani <S01221810@acad.tri-c.edu>
2019-11-03 22:40:28 -0500

    added comment

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -31,6 +31,18 @@ namespace Lab2
             Console.WriteLine("HelloWorld!   From Mercy Nani");
             Console.ReadLine();
 
+            /* (a)If the program contains no errors the Console application executes when you press start or Ctrl + F5 
+             * (b)Integer variables are whole numbers that can have both zero, positive and negative values but no decimal values. For example, 0, -5, 10
+             *    Double/floating-point variabales can hold real numbers (numbers that can have a fractional part). Floats are used when more precision is needed.
+             * (c)The GetName Method retrieves the name of the constant in the specified enumeration that has the specified value.
+             * (d)To read the value of an instance variable, we create method called a get accessor. To assign a value ton instance variable, we use a method called a set accessor.
+             * (e)A class is the building block of C#; it holds its own data members and functions.It is like a blueprint that specifies what the type can do.
+             *    An object is basically a block of memory that has been allocated and configured according to the blueprint.
+             *    The class is a technique used to bind data and its associated functions together, in contrast, Object is the created instance of a class.
+             *   
+             *   We can create multiple instance of a class.
+             */
+
         }
     }
 }
