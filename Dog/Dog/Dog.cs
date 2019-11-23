a1e1b860a3d4371c247d9febe30c2b614a12b5ae
Mercy Nani <S01221810@acad.tri-c.edu>
2019-11-22 20:39:28 -0500

    lab5

---
+++ Dog/Dog/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public string trickName { get; set; }
+
+        // Add bark() method
+
+        public void bark()
+        {
+            Console.WriteLine("{0} is Barking...", Name);
+            Console.ReadLine();
+        }
+
+        // Add doTrick() method 
+        public void doTrick(string trickName)
+        {
+            Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", Name, trickName);
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
--- Dog/Dog/Properties/AssemblyInfo.cs
+++ Dog/Dog/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab5")]
+[assembly: AssemblyTitle("Dog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("Lab5")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dog")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2fac24a3-f4f0-4536-b962-803ef77f738d")]
+[assembly: Guid("0fe66fd8-3cf0-4e34-bdbe-8fd93e6bc350")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
