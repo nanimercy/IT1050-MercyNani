ff78ac0c8c24292b8534b243402f72e58c8bd24b
Mercy Nani <S01221810@acad.tri-c.edu>
2019-12-09 12:33:34 -0500

    Lab 7 commit

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -8,9 +8,36 @@ namespace Lab7
 {
     class Program
     {
+        //created an object mybook to access Book class method and properties
         static void Main(string[] args)
         {
-
+            Book myBook = new Book();
+            myBook.Title = "On Earth We're Briefly Gorgeous";
+            myBook.Author = "Ocean Vuong";
+            myBook.Year = 2019;
+            myBook.Display();
         }
     }
+
+    // Access specifier & properties
+   public class Book
+   { 
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int Year { get; set; }
+
+      //Method
+      public void Display()
+        {
+
+            Console.WriteLine("Title: {0}\nAuthor: {1}\nYear: {2}",
+                Title, Author, Year);
+            Console.ReadLine();
+        }
+   }
+
+
+
+
 }
+    
---
+++ Lab7Question2/Lab7Question2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookTest myBook = new BookTest();
+            Console.WriteLine(myBook.Title);
+            Console.WriteLine(myBook.Author);
+            Console.WriteLine(myBook.Year);
+            Console.ReadLine();
+        }
+    }
+    public class BookTest
+    {
+            public string Title, Author, Year;
+            //Constructor
+            public BookTest()
+            {
+                Title = "On Earth We're Briefly Gorgeous";
+                Author = "Ocean Vuong";
+                Year = "2019";
+            }
+    }
+
+
+    
+}
